dead9cf5ba859f258a0ebc5f4f36af4cb5f43405
superhsu <31141265+superhsu@users.noreply.github.com>
2018-01-17 11:44:01 -0800

    functional que and stack done

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,17 +2,22 @@ var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
-
+  var storage = {}; 
+  var count = 0; 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    count++;
   };
 
   someInstance.dequeue = function() {
+    if (count === 0) { count = 0; } else { 
+      count -= 1; 
+    }
   };
 
   someInstance.size = function() {
+    return count; 
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,23 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0; 
 
   // Implement the methods below
   someInstance.push = function(value) {
+    count++; 
   };
 
   someInstance.pop = function() {
+    if (count === 0) {
+      return 0; 
+    } else {
+      count -= 1; 
+    }
   };
 
   someInstance.size = function() {
+    return count; 
   };
 
   return someInstance;
