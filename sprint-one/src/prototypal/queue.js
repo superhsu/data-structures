c8879f58c89886f5db7554f79561ed9a13a4e82b
superhsu <31141265+superhsu@users.noreply.github.com>
2018-01-17 19:36:30 -0800

    completed proto and pseudo types

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,35 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQue = Object.create(queueMethods); 
+  newQue.storage = {}; 
+  newQue.count = 0; 
+  newQue.dequePos = 0; 
+  newQue.enquePos = 0; 
+
+  return newQue; 
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this.storage[this.enquePos] = value; 
+  this.count++;
+  this.enquePos++;
+};
+
+queueMethods.dequeue = function() {
+  if (this.count > 0) {
+    var temp = this.storage[this.dequePos];
+    delete this.storage[this.dequePos];
+    this.count--; 
+    this.dequePos++; 
+    return temp;
+  }
+};
+
+queueMethods.size = function() {
+  return this.count;
+};
 
 
--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+   
+  this.storage = {}; 
+  this.count = 0; 
+  this.dequePos = 0; 
+  this.enquePos = 0; 
+
+  
 };
 
+var queueMethods = {};
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.enquePos] = value; 
+  this.count++;
+  this.enquePos++;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.count > 0) {
+    var temp = this.storage[this.dequePos];
+    delete this.storage[this.dequePos];
+    this.count--; 
+    this.dequePos++; 
+    return temp;
+  }
+};
+
+Queue.prototype.size = function() {
+  return this.count;
+};
