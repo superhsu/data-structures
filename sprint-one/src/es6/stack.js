49b5949889ea5ddda3a8157b1414da2c6d5b95bc
superhsu <31141265+superhsu@users.noreply.github.com>
2018-01-17 20:05:22 -0800

    finished es6 commit

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,26 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
-  }
+  	this.count = 0; 
+  	this.storage = {}; 
+  };
 
-}
+  push (value) {
+    this.storage[this.count] = value;
+    this.count++; 
+  };
+
+  pop () {
+  	if (this.count > 0) {
+      this.count--;
+  	  var temp = this.storage[this.count];
+      delete this.storage[this.count];
+      return temp; 
+    }
+  };
+
+  size () {
+  	return this.count; 
+  };
+
+};
